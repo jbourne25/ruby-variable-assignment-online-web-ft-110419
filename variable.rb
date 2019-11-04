c2d4ed3ca577dd86f90832a5fdd007d3bfc942fc
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-04 18:29:09 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,9 +1,4 @@
  # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-
-#def greeting
- # local_variables = 'Hello World'
-#greeting
-#end
+ # give it the value of the string "Hello World
 
 greeting = "Hello World"
